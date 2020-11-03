7f752e38caae70c6ae53a5275f2a976fed266bb8
cyrus25 <cyruspassi@gmail.com>
2020-11-03 22:42:34 +0530

    remove commented code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var socket = require("socket.io");
+const { v4: uuidv4 } = require("uuid");
 const PatchManager = require("./PatchManager");
 const { SyncStateRemote } = require("@syncstate/remote-server");
 const remote = new SyncStateRemote();
@@ -9,29 +10,36 @@ var server = app.listen(8000, function () {
 });
 
 var io = socket(server);
+var projectId = uuidv4();
 
 var patchManager = new PatchManager();
 
 io.on("connection", async (socket) => {
   socket.on("fetchDoc", (path) => {
-    const patchesList = patchManager.getAllPatches(1, path);
+    //get all patches
+    const patchesList = patchManager.getAllPatches(projectId, path);
 
     if (patchesList) {
+      //send each patch to the client
       patchesList.forEach((change) => {
         socket.emit("change", path, change);
       });
-      // remote.loadPatches(patchesList);
-      // socket.emit("loaded", path);
     }
   });
 
+  //patches recieved from the client
   socket.on("change", (path, change) => {
     change.origin = socket.id;
+
+    //resolves conflicts internally
     remote.processChange(socket.id, path, change);
   });
 
   const dispose = remote.onChangeReady(socket.id, (path, change) => {
-    patchManager.store(1, path, change);
+    //store the patches in js runtime or a persistent storage
+    patchManager.store(projectId, path, change);
+
+    //broadcast the pathes to other clients
     socket.broadcast.emit("change", path, change);
   });
 });
